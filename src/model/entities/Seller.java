37d76ee657ba56775a19ea715eb983f1151088f9
Jonathan Aires <airesjonathan123@gmail.com>
2024-01-09 22:38:11 -0300

    Department and Seller classes

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,70 @@
+package model.entities;
+
+import java.sql.Date;
+
+public class Seller {
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
+	private Department deparment;
+	
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.deparment = department;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	
+	public Department getDeparment() {
+		return deparment;
+	}
+
+	public void setDeparment(Department deparment) {
+		this.deparment = deparment;
+	}
+}
