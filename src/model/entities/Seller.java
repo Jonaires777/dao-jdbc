669d2bec65b5e4f305d677364b815308d22e03c5
Jonathan Aires <airesjonathan123@gmail.com>
2024-01-09 22:45:33 -0300

    Updates in Seller class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,9 +1,12 @@
 package model.entities;
 
-import java.sql.Date;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
-public class Seller {
+public class Seller implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String name;
 	private String email;
@@ -67,4 +70,29 @@ public class Seller {
 	public void setDeparment(Department deparment) {
 		this.deparment = deparment;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", deparment=" + deparment + "]";
+	}
+	
+	
 }
